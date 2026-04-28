6e7d231baf3ee9c6c120e28face676962e057abc
UidUid技術 <hogamehk@gmail.com>
2026-04-29 01:56:35 +0800

    Add files via upload

---
+++ js/tawk.js
@@ -0,0 +1,15 @@
+// Tawk.to 在線客服 - 全站通用
+// 在任意頁面加入 <script src="js/tawk.js"></script> 即可啟用右下角客服氣泡
+(function () {
+    var Tawk_API = window.Tawk_API || {};
+    var Tawk_LoadStart = new Date();
+    window.Tawk_API = Tawk_API;
+
+    var s1 = document.createElement('script');
+    var s0 = document.getElementsByTagName('script')[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/5e145a9527773e0d832c3ff6/default';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
